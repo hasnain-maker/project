f7d072f54d31a99545dfc37b3b1221124b3c799f
Hasnain Alvi <alvihasnain305@gmail.com>
2024-11-17 19:21:12 +0500

    infowebusa
    good

---
+++ infowebusa/app.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementById('nav-toggle');
+const nav = document.getElementById('nav');
+toggleButton.addEventListener('click',()=>{
+
+    nav.classList.toggle('active');
+
+})
+
+
+
+
+
+
+
+
+
